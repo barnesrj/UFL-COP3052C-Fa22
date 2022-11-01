f0cb69714cdf773381826bce042c43e3d63432e0
barnesrj <116855852+barnesrj@users.noreply.github.com>
2022-10-31 20:49:11 -0400

    Update github Lab file.py
    Added subtraction

--- github Lab file.py
+++ github Lab file.py
@@ -1,4 +1,12 @@
 first_num = float(input("Input first number: "))
 second_num = float(input("Input second number: "))
-result = first_num + second_num
+operation = int(input("Choose operation:, either 1. Addition or 2. Subtraction"))
+while operation != 1 and operation != 2:
+    print("try again! ")
+    operation = int(input("Choose operation:, either 1. Addition or 2. Subtraction "))
+if operation == 1:
+    result = first_num + second_num
+elif operation == 2:
+    result = first_num - second_num
+
 print(result)
